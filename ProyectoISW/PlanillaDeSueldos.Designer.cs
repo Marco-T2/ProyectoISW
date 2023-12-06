1b57e406ca800e42cf4a5deae5027b45c2043707
German Miranda <germanmiranda0593@gmail.com>
2023-12-06 15:09:54 -0400

    Reporte en excel de planilla de sueldos y salarios

--- ProyectoISW/PlanillaDeSueldos.Designer.cs
+++ ProyectoISW/PlanillaDeSueldos.Designer.cs
@@ -41,6 +41,7 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.btnGenerarR = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -76,7 +77,8 @@
             dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
             this.dataGridView1.EnableHeadersVisualStyles = false;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 199);
+            this.dataGridView1.Location = new System.Drawing.Point(9, 162);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersVisible = false;
@@ -84,7 +86,7 @@
             this.dataGridView1.RowTemplate.Height = 40;
             this.dataGridView1.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(1057, 392);
+            this.dataGridView1.Size = new System.Drawing.Size(793, 318);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellPainting += new System.Windows.Forms.DataGridViewCellPaintingEventHandler(this.dataGridView1_CellPainting);
             // 
@@ -94,20 +96,22 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label1.Location = new System.Drawing.Point(313, 129);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(235, 105);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(456, 29);
+            this.label1.Size = new System.Drawing.Size(364, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "PLANILLA DE SUELDOS Y SALARIOS";
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.btnGenerarR);
             this.groupBox1.Controls.Add(this.dateTimePicker1);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Location = new System.Drawing.Point(43, 26);
+            this.groupBox1.Location = new System.Drawing.Point(32, 21);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1004, 86);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(753, 70);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "INFORMACION DE LA PLANILLA DE SUELDOS Y SALARIOS";
@@ -115,12 +119,13 @@
             // dateTimePicker1
             // 
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(18, 51);
+            this.dateTimePicker1.Location = new System.Drawing.Point(14, 41);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dateTimePicker1.MaxDate = new System.DateTime(3000, 12, 31, 0, 0, 0, 0);
             this.dateTimePicker1.MinDate = new System.DateTime(1800, 1, 1, 0, 0, 0, 0);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.dateTimePicker1.Size = new System.Drawing.Size(171, 22);
+            this.dateTimePicker1.Size = new System.Drawing.Size(129, 20);
             this.dateTimePicker1.TabIndex = 5;
             this.dateTimePicker1.Value = new System.DateTime(2023, 11, 25, 15, 25, 23, 0);
             this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
@@ -130,9 +135,10 @@
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 32);
+            this.label2.Location = new System.Drawing.Point(11, 26);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(196, 16);
+            this.label2.Size = new System.Drawing.Size(160, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "SELECCIONAR EL MES Y AÑO";
             // 
@@ -140,10 +146,10 @@
             // 
             this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(587, 158);
+            this.label3.Location = new System.Drawing.Point(440, 128);
             this.label3.Margin = new System.Windows.Forms.Padding(0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(18, 16);
+            this.label3.Size = new System.Drawing.Size(16, 13);
             this.label3.TabIndex = 13;
             this.label3.Text = "M";
             // 
@@ -151,10 +157,10 @@
             // 
             this.label8.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(383, 158);
+            this.label8.Location = new System.Drawing.Point(287, 128);
             this.label8.Margin = new System.Windows.Forms.Padding(0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(194, 16);
+            this.label8.Size = new System.Drawing.Size(157, 13);
             this.label8.TabIndex = 12;
             this.label8.Text = "CORRESPONDIENTE AL MES";
             // 
@@ -162,10 +168,10 @@
             // 
             this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(608, 158);
+            this.label4.Location = new System.Drawing.Point(456, 128);
             this.label4.Margin = new System.Windows.Forms.Padding(0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(26, 16);
+            this.label4.Size = new System.Drawing.Size(22, 13);
             this.label4.TabIndex = 14;
             this.label4.Text = "DE";
             // 
@@ -173,10 +179,10 @@
             // 
             this.label5.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(641, 158);
+            this.label5.Location = new System.Drawing.Point(481, 128);
             this.label5.Margin = new System.Windows.Forms.Padding(0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(16, 16);
+            this.label5.Size = new System.Drawing.Size(14, 13);
             this.label5.TabIndex = 15;
             this.label5.Text = "A";
             // 
@@ -185,17 +191,28 @@
             this.label6.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(465, 174);
+            this.label6.Location = new System.Drawing.Point(349, 141);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(150, 15);
+            this.label6.Size = new System.Drawing.Size(129, 13);
             this.label6.TabIndex = 6;
             this.label6.Text = "(Expresado en Bolivianos)";
             // 
+            // btnGenerarR
+            // 
+            this.btnGenerarR.Location = new System.Drawing.Point(207, 26);
+            this.btnGenerarR.Name = "btnGenerarR";
+            this.btnGenerarR.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerarR.TabIndex = 6;
+            this.btnGenerarR.Text = "Generar reporte";
+            this.btnGenerarR.UseVisualStyleBackColor = true;
+            this.btnGenerarR.Click += new System.EventHandler(this.btnGenerarR_Click);
+            // 
             // PlanillaDeSueldos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1081, 695);
+            this.ClientSize = new System.Drawing.Size(811, 565);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.groupBox1);
@@ -205,7 +222,6 @@
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label8);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "PlanillaDeSueldos";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "PlanillaDeSueldos";
@@ -228,5 +244,6 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnGenerarR;
     }
 }
